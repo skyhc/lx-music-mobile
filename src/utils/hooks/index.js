9080eb0a2a22fdde03e032d0a11d05e405647040
skyhc <61585718+skyhc@users.noreply.github.com>
2026-09-12 02:55:11 +0800

    feat(ipad): export layout size hook

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -2,6 +2,7 @@ export { default as useLayout } from './useLayout'
 export { default as useKeyboard } from './useKeyboard'
 export { default as useWindowSize } from './useWindowSize'
 export { default as useHorizontalMode } from './useHorizontalMode'
+export { default as useLayoutSize } from './useLayoutSize'
 export { default as useDeviceOrientation } from './useDeviceOrientation'
 
 // export { default as usePlayTime } from './usePlayTime'
