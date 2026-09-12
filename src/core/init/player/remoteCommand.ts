4bfe338df6e9c01fa69d9e506dfb04283e04e2ce
skyhc <61585718+skyhc@users.noreply.github.com>
2026-09-12 20:04:25 +0800

    feat(ipad): route Magic Keyboard navigation shortcuts

--- src/core/init/player/remoteCommand.ts
+++ src/core/init/player/remoteCommand.ts
@@ -1,6 +1,7 @@
 import { onRemoteCommand } from '@/utils/nativeModules/utils'
 import { pause, play, playNext, playPrev, togglePlay } from '@/core/player/player'
 import { markTimeoutExitInteraction } from '@/core/player/timeoutExit'
+import { setNavActiveId } from '@/core/common'
 
 export default () => {
   onRemoteCommand((event) => {
@@ -26,6 +27,13 @@ export default () => {
         if (typeof event.position == 'number') {
           global.app_event.setProgress(event.position)
         }
+        break
+      case 'nav_search':
+      case 'nav_songlist':
+      case 'nav_top':
+      case 'nav_love':
+      case 'nav_setting':
+        setNavActiveId(event.command)
         break
     }
   })
