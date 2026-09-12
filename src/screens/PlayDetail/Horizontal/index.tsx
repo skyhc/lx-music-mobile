562e6a75bb23da57d3641517fddcfcc4f8058803
skyhc <61585718+skyhc@users.noreply.github.com>
2026-09-12 21:25:50 +0800

    feat(ipad): adopt compact integrated play detail layout

--- src/screens/PlayDetail/Horizontal/index.tsx
+++ src/screens/PlayDetail/Horizontal/index.tsx
@@ -2,25 +2,24 @@ import { memo, useEffect } from 'react'
 import { View, AppState } from 'react-native'
 import { screenkeepAwake, screenUnkeepAwake } from '@/utils/nativeModules/utils'
 import StatusBar from '@/components/common/StatusBar'
-import MoreBtn from './MoreBtn'
-
 import Header from './components/Header'
 import { setComponentId } from '@/core/common'
 import { COMPONENT_IDS } from '@/config/constant'
 import PageContent from '@/components/PageContent'
 import commonState, { type InitState as CommonState } from '@/store/common/state'
-
 import Pic from './Pic'
-// import ControlBtn from './ControlBtn'
 import Lyric from './Lyric'
 import Player from './Player'
 import { createStyle } from '@/utils/tools'
 import { marginLeftRaw } from './constant'
 import { useStatusbarHeight } from '@/store/common/hook'
-// import MoreBtn from './MoreBtn2'
+import { useWindowSize } from '@/utils/hooks'
+import { isIPadWindowed } from '@/utils/ipadWindow'
 
 export default memo(({ componentId }: { componentId: string }) => {
   const statusBarHeight = useStatusbarHeight()
+  const windowSize = useWindowSize()
+  const windowedIPad = isIPadWindowed(windowSize.width, windowSize.height)
 
   useEffect(() => {
     setComponentId(COMPONENT_IDS.playDetail, componentId)
@@ -52,20 +51,15 @@ export default memo(({ componentId }: { componentId: string }) => {
   }, [])
 
   return (
-    <PageContent>
+    <PageContent integrateWindowControls>
       <StatusBar />
-      <View style={{ ...styles.container, paddingTop: statusBarHeight }}>
+      <View style={{ ...styles.container, paddingTop: windowedIPad ? 0 : statusBarHeight }}>
         <View style={styles.left}>
           <Header />
           <View style={styles.leftContent}>
-            <MoreBtn />
             <Pic componentId={componentId} />
           </View>
           <Player />
-          {/* <View style={styles.controlBtn} nativeID="pageIndicator">
-            <MoreBtn />
-            <ControlBtn />
-          </View> */}
         </View>
         <View style={styles.right}>
           <Lyric />
@@ -84,26 +78,15 @@ const styles = createStyle({
     flex: 1,
     width: '45%',
     paddingBottom: 10,
-    // backgroundColor: 'rgba(0,0,0,0.1)',
   },
   leftContent: {
     flexShrink: 1,
     flexGrow: 0,
     marginLeft: marginLeftRaw,
-    // flexDirection: 'row',
-    // backgroundColor: 'rgba(0,0,0,0.1)',
-    // alignItems: 'center',
   },
   right: {
     width: '55%',
     flexGrow: 0,
     flexShrink: 0,
-  },
-  controlBtn: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    // backgroundColor: '#eee',
   },
 })
