932d75cbdefdecfc561e7674a3c3575aa565a5d2
skyhc <61585718+skyhc@users.noreply.github.com>
2026-09-12 21:25:35 +0800

    feat(ipad): stack play detail controls below artwork

--- src/screens/PlayDetail/Horizontal/Player/index.tsx
+++ src/screens/PlayDetail/Horizontal/Player/index.tsx
@@ -1,19 +1,20 @@
 import { memo } from 'react'
 import { View } from 'react-native'
-
-// import Title from './components/Title'
 import { createStyle } from '@/utils/tools'
 import { NAV_SHEAR_NATIVE_IDS } from '@/config/constant'
 import PlayInfo from './PlayInfo'
 import ControlBtn from './ControlBtn'
 import { marginLeftRaw } from '../constant'
-
+import SongInfo from '../components/SongInfo'
+import ActionBar from '../components/ActionBar'
 
 export default memo(() => {
   return (
     <View style={styles.container} nativeID={NAV_SHEAR_NATIVE_IDS.playDetail_player}>
-      <ControlBtn />
+      <SongInfo />
+      <ActionBar />
       <PlayInfo />
+      <ControlBtn />
     </View>
   )
 })
@@ -23,7 +24,5 @@ const styles = createStyle({
     flexShrink: 0,
     flexGrow: 1,
     marginLeft: marginLeftRaw,
-    // paddingRight: 15,
-    // backgroundColor: 'rgba(0,0,0,0.1)',
   },
 })
